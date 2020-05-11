9f09418e1199d44eb085f3900352944971841792
Carlos21122000 <craxtum1@gmail.com>
2020-05-10 22:07:27 -0600

    carga

---
+++ Lab6Juegos/Assets/Scripts/eliminar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eliminar : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy1"))
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.CompareTag("Coin1"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
